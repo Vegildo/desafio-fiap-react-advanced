d2704fec303554b2db79ea50413c3c77da250c82
Vegildo <leobss12@gmail.com>
2025-03-07 17:31:42 -0300

    feat: first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## 👋  Bem-vindo(a)
+Olá, meu nome é Leonardo Braga. E esse é o desafio para Pós-graduação da FIAP, para FullStack.
+
+## 🎉 Awesome Desafio REACT-ADVANCED
+
+### 💻 Tecnologias Utilizadas
+
+[![React](https://shields.io/badge/react-black?logo=react&style=for-the-badge)](https://react.dev/)
+[![JavaScript](https://img.shields.io/badge/javascript-gray?logo=javascript&logoColor=yellow&style=for-the-badge)](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+[![Vite](https://img.shields.io/badge/Vite-646CFF?logo=Vite&logoColor=yellow&style=for-the-badge)](https://vite.dev/)
+
+### 👓  Como utilizar
+
+1. Clone este repositório para sua máquina local.
+2. No terminal do projeto rode `npm install`.
+3. Ainda no mesmo termonal rode `npm test`.
+4. Observe o resultado dos testes.
+
+**Ou**
+
+1. Clone este repositório para sua máquina local.
+2. No terminal do projeto rode `npm install`.
+3. Ainda no mesmo termonal rode `npm run dev`.
+4. Abra o navegador na URL `http://localhost:5173`.
+5. Teste o carrinho adcionando e deletando itens.
+
+
+
+### 🔍 Créditos
+
+Este app foi desenvolvido, por Leonardo Braga, como parte de um desafio educacional da Faculdade de Informática e Administração Paulista.
+
+---
+
+Lembre-se de conferir o repositório original [aqui](https://github.com/Vegildo/desafio-fiap-react-advanced) e deixar uma ⭐️ se você gostou do projeto!
---
+++ src/context/CartContext.jsx
@@ -0,0 +1,68 @@
+import { createContext, useContext, useState } from "react";
+
+const initialCartproducts = [
+  {
+      id: 1,
+      name: "Woman Sleeve 003",
+      imageSrc: "/products/woman-short-sleeve-003.png",
+      price: 79.99,
+      size: "XL",
+      color: "Blue"
+  },
+  {
+      id: 2,
+      name: "Men Shirt 001",
+      imageSrc: "/products/men-shirt-001.png",
+      price: 89.99,
+      size: "L",
+      color: "Gray"
+  }
+];
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+
+  const [products, setProducts] = useState(initialCartproducts);
+
+  const addToCart = (prod) => {
+    setProducts((oldState) => {
+      const isProductInCart = oldState.some((item) => item.id === prod.id);
+      if (!isProductInCart) {
+        return [...oldState, prod];
+      }
+      return oldState;
+    });
+  };
+
+  const removeFromCart = (prodID) => {
+    setProducts((oldState) => {
+      return [...oldState.filter((item) => item.id != prodID)];
+    });
+  };
+
+  const getCartTotalValue = () => {
+    return products.reduce((total, product) => total + product.price, 0).toFixed(2);
+  };
+
+  const isProductInCart = (product) => {
+    return products.some((item) => item.id === product.id);
+  };
+
+  return (
+    <CartContext.Provider value={{
+      products,
+      addToCart,
+      removeFromCart,
+      getCartTotalValue,
+      isProductInCart
+    }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCartContext = () => {
+  return useContext(CartContext);
+};
+
